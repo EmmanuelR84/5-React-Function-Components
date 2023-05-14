3ca96345b7a08754136fd2427724e696985a5184
Emmanuel <emmanuelramello@gmail.com>
2023-05-14 11:49:48 -0300

    Usando typography, biblioteca material ui, manejando el evento submit, recibir datos del formulario

--- componentes-funcionales/src/App.js
+++ componentes-funcionales/src/App.js
@@ -1,13 +1,20 @@
 //No hay necesidad de colocar las tags HTML (gracias a los fragments).
 import './App.css';
+import Container from '@mui/material/Container';
 import FormSignUp from './Components/FormSignUp';
+import Typography from '@mui/material/Typography';
 
 function App() {
+
+  const handleSubmit = ( valores ) => {
+    console.log('APPJS: ', valores);
+  }
+
   return (
-    <>
-    <h1>Formulario Registro</h1>
-      <FormSignUp />
-    </>
+    <Container component='section' maxWidth='sm' >
+    <Typography variant='h3' align='center' >Formulario Registro</Typography>
+      <FormSignUp handleSubmit={handleSubmit} />
+    </Container>
   );
 }
 
--- componentes-funcionales/src/Components/FormSignUp.jsx
+++ componentes-funcionales/src/Components/FormSignUp.jsx
@@ -1,18 +1,74 @@
+import { useState } from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
-function FormSignUp () {
-  return <form>
-    <label>Nombre</label>
-    <input type="text"/>
-    <label>Apellido</label>
-    <input type="text"/>
-    <label>Correo electronico</label>
-    <input type="email"/>
-    <label>Promociones</label>
-    <input type="checkbox"/>
-    <label>Novedades</label>
-    <input type="checkbox"/>
-    <Button variant='contained' >Registrarse</Button>
+function FormSignUp({ handleSubmit }) {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [prom, setProm] = useState(true);
+  const [nov, setNov] = useState(true);
+
+  return <form onSubmit={(e) => {
+    e.preventDefault();
+    handleSubmit({ name, lastName, email, prom, nov });
+  }}>
+    <TextField
+      required
+      id='name'
+      label='Nombres'
+      variant='outlined'
+      fullWidth
+      margin='normal'
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    />
+    <TextField
+      required
+      id='lastName'
+      label='Apellido'
+      variant='outlined'
+      fullWidth
+      margin='normal'
+      value={lastName}
+      onChange={(e) => setLastName(e.target.value)}
+    />
+    <TextField
+      required
+      id='email'
+      label='Email'
+      variant='outlined'
+      fullWidth
+      margin='normal'
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+    />
+
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={prom}
+            onChange={(e) => {
+              setProm(e.target.checked)
+            }}
+          />
+        } label='Promociones' />
+      <FormControlLabel 
+        control={
+          <Switch
+            checked={nov}
+            onChange={(e) => {
+              setNov(e.target.checked)
+            }}
+          />
+          } label='Novedades' />
+    </FormGroup>
+
+    <Button variant='contained' type='submit' >Registrarse</Button>
   </form>
 }
 
